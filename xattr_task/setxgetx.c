536cc9a18a6156aa2ce1bb0bece26b52c87b5bb6
Sachin Kumar <meetsac92@gmail.com>
2015-07-09 16:51:23 +0530

    Updated patch of xattr task /*  *Fixed reviews */
    Signed-off-by: Sachin Kumar <meetsac92@gmail.com>

--- xattr_task/setxgetx.c
+++ xattr_task/setxgetx.c
@@ -1,29 +1,71 @@
 # include <stdio.h>
+# include <stdlib.h>
 # include <string.h>
 # include <sys/types.h>
+# include <errno.h>
 # include <attr/xattr.h>
 
+/* Program to check the proper working of setxattr and getxattr */
 int
 main(int argc,char *argv[])
 {
-	char var1[100];
-        size_t size=0;
-        int val=atoi(argv[3]);
+        char *attr_name;
+        size_t size = 0;
+        int val,i;
+        FILE *fp;
         if(argc==4){
-		strcpy(var1,"trusted.");
-		strcat(var1,argv[2]);
-		size = setxattr(argv[1],var1,&val,sizeof(val),0);
-		if(size == 0) {
-			printf ("\nAttribute is Set\n");
-			val =0;      
-			size = getxattr(argv[1],var1,&val,sizeof(val));
-			if(size > 0)
-				printf ("Attribute value retrieved.\n\n%s = %d\n", var1 , val);
-			else
-				printf ("Error in Retrieval\n");
-		} else
-			printf ("Error in Setting Attribute\n\n");
+                fp=fopen(argv[1],"r");
+                 
+                if(fp==NULL){
+                        printf("%s",strerror(errno));
+                        goto out;
+                }
+                
+                for(i=0;i<strlen(argv[3]);i++) {
+                        if(argv[3][i]<'0' || argv[3][i]>'9') {
+                                printf("pass the proper arguments\n\n");
+                                goto out;
+                        }
+                }
+                
+                val = atoi(argv[3]);
+                attr_name = (char *) malloc ((sizeof (char) * (strlen(argv[3]) + 9)));
+                strcpy(attr_name,"trusted.");
+                strcat(attr_name,argv[2]);
+                /*new attribute is added
+                * INPUT :
+                *      char *argv[1]       :       filename
+                *      char *attr_name     :       attribute name
+                *      void *val           :       pointer to attribute value
+                *      size_t sizeof(val)  :       size of value
+                *      int flag            :       flag value
+                * OUTPUT :
+                *      size_t              :       success/failure indicator
+                */
+                size = setxattr(argv[1],attr_name,&val,sizeof(val),0);
+                if(size == 0) {
+                        printf ("\nAttribute is added here\n");
+                        val =0;
+                        /*existing attribute value
+                        *INPUT :
+                        *      char *argv[1]      :       filename
+                        *      char *attr_name    :       attribute name
+                        *      void *val          :       pointer to 
+                        *                                 attribute value
+                        *      size_t sizeof(val) :       size of value
+                        * OUTPUT :
+                        *      size_t             :       success/failure
+                        *                                 indicator
+                        */
+                        size = getxattr(argv[1],attr_name,&val,sizeof(val));
+                        if(size > 0)
+                                printf ("Attribute value retrieved.\n\n%s = %d\n", attr_name , val);
+                        else
+                                printf ("Error while accessing the existing attribute value\n");
+                } else
+                        printf ("Error While Setting the new Attribute\n\n");
         }
         else
-                printf("pleas give the proper number of arguments");
+                printf("Please give the proper number of arguments");
+out:        return 0;
 }
